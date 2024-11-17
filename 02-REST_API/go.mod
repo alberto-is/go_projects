1681bc3c8127d91feb6d55060d0a1e1238df07b9
alberto-is <albertobarrais12@gmail.com>
2024-11-17 10:03:20 +0100

    chore: Init basic Go REST API project
    A simple "Hola Mundo" to start with the API

---
+++ 02-REST_API/go.mod
@@ -0,0 +1,3 @@
+module 02-REST_API
+
+go 1.23.3
---
+++ 02-REST_API/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hola Mundo")
+}
+
+func main() {
+	var err error
+
+	http.HandleFunc("/", greet)
+	server := http.Server{
+		Addr: ":8080",
+	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
