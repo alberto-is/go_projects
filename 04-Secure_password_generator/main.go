95c3ae7a58b6e72204f0b02e2588ef1d9af099a9
alberto-is <albertobarrais12@gmail.com>
2024-11-17 21:21:20 +0100

    feat: Enhance flag help text

--- 04-Secure_password_generator/main.go
+++ 04-Secure_password_generator/main.go
@@ -5,22 +5,62 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+)
+
+var (
+	length         int
+	includeSpecial bool
+	includeNumbers bool
+	includeUpper   bool
+	marks          bool
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
 
-	length := flag.Int("length", 12, "Longitud de la contraseña (mínimo 4)")
-	includeSpecial := flag.Bool("special", false, "Incluir caracteres especiales")
-	includeNumbers := flag.Bool("numbers", true, "Incluir números")
-	includeUpper := flag.Bool("upper", true, "Incluir letras mayúsculas")
-	marks := flag.Bool("marks", true, "Incluir putnos y comas")
+Generador de Contraseñas Seguras v1.0
+------------------------------------
+Uso: %s [opciones]
+
+Opciones:
+	-l, --length  int     Longitud de la contraseña (mínimo 4) (default 12)
+	-s, --special         Incluir caracteres especiales
+	-n, --numbers         Incluir números (default true)
+	-u, --upper          Incluir letras mayúsculas (default true)
+	-m, --marks          Incluir puntos y comas (default true)
+
+Ejemplos:
+	%s -l 16             # Contraseña de 16 caracteres
+	%s --length 16 -s    # 16 caracteres con especiales
+
+	z
+`, os.Args[0], os.Args[0], os.Args[0])
+	}
+
+	flag.IntVar(&length, "length", 12, "Longitud de la contraseña (mínimo 4)")
+	flag.IntVar(&length, "l", 12, "Longitud de la contraseña (mínimo 4)")
+
+	flag.BoolVar(&includeSpecial, "special", false, "Incluir caracteres especiales")
+	flag.BoolVar(&includeSpecial, "s", false, "Incluir caracteres especiales")
+
+	flag.BoolVar(&includeNumbers, "numbers", true, "Incluir números")
+	flag.BoolVar(&includeNumbers, "n", true, "Incluir números")
+
+	flag.BoolVar(&includeUpper, "upper", true, "Incluir letras mayúsculas")
+	flag.BoolVar(&includeUpper, "u", true, "Incluir letras mayúsculas")
+
+	flag.BoolVar(&marks, "marks", true, "Incluir puntos y comas")
+	flag.BoolVar(&marks, "m", true, "Incluir puntos y comas")
+
 	flag.Parse()
 
-	if *length < 4 {
+	if length < 4 {
 		log.Fatal("La longitud mínima de la contraseña debe ser 4")
 	}
 	// Generar la contraseña
-	password, err := caractergen.GenRandomPassword(*length, *includeSpecial, *includeNumbers, *includeUpper, *marks)
+	password, err := caractergen.GenRandomPassword(length, includeSpecial, includeNumbers, includeUpper, marks)
 	if err != nil {
 		log.Fatalf("Error generando la contraseña: %v", err)
 	}
