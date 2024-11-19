1cf2b2cd67189624810a75acbc7086b222c2c9e0
alberto-is <albertobarrais12@gmail.com>
2024-11-20 00:20:10 +0100

    chore: 07-evolutionary-algorithm DONE

--- 07-evolutionary-algorithm/evol.go
+++ 07-evolutionary-algorithm/evol.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
 )
 
-const target string = "Hola mundo"
+const target string = "En un lugar de la Mancha de cuyo nombre no quiero acordarme no ha mucho tiempo que vivia un hidalgo de los de lanza en astillero adarga antigua rocin flaco y galgo corredor"
+
 const targetLength int = len(target)
 
 const populationSize int = 100
 
 const mutacion float32 = 0.01
+
+const generations int = 10000000
 
 type Individual struct {
 	chros   string
@@ -55,43 +59,85 @@ func createInitialPopulation(random *rand.Rand) []Individual {
 }
 
 func seleccion(random *rand.Rand, population *[]Individual) [2]Individual {
-	var bestCandidate1 Individual
-	bestFitness1 := -1
+	candidateChan := make(chan Individual, 2)
+	defer close(candidateChan)
 
-	for i := 0; i < 5; i++ {
-		candidate := (*population)[random.Intn(populationSize)]
-		if candidate.fitness > bestFitness1 {
-			bestCandidate1 = candidate
-			bestFitness1 = candidate.fitness
+	findBest := func() {
+		bestFitness := -1
+		var bestCandidate Individual
+		for i := 0; i < 5; i++ {
+			candidate := (*population)[random.Intn(populationSize)]
+			if candidate.fitness > bestFitness {
+				bestCandidate = candidate
+				bestFitness = candidate.fitness
+			}
+		}
+		candidateChan <- bestCandidate
+	}
+
+	go findBest()
+	go findBest()
+
+	return [2]Individual{<-candidateChan, <-candidateChan}
+}
+
+func crossover(random *rand.Rand, parent1, parent2 Individual) Individual {
+	crossoverPoint := random.Intn(targetLength)
+	child := Individual{
+		chros: parent1.chros[:crossoverPoint] + parent2.chros[crossoverPoint:],
+	}
+	child.fitness = fitnessFunc(child.chros)
+	return child
+}
+
+func mutate(random *rand.Rand, individual Individual) Individual {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ")
+	chros := []rune(individual.chros)
+
+	for i := range chros {
+		if random.Float32() < mutacion {
+			chros[i] = letters[random.Intn(len(letters))]
 		}
 	}
 
-	var bestCandidate2 Individual
-	bestFitness2 := -1
-	for i := 0; i < 5; i++ {
-		candidate := (*population)[random.Intn(populationSize)]
-		if candidate.fitness > bestFitness2 {
-			bestCandidate2 = candidate
-			bestFitness2 = candidate.fitness
-		}
+	mutated := Individual{
+		chros: string(chros),
 	}
-
-	return [2]Individual{bestCandidate1, bestCandidate2}
-}
-
-func crossover() {
-
-}
-
-func mutate() {
-
-}
-
-func updatePopulation() {
-
+	mutated.fitness = fitnessFunc(mutated.chros)
+	return mutated
 }
 
 func main() {
 	random := rand.New(rand.NewSource(time.Now().UnixMicro()))
-	createInitialPopulation(random)
+	population := createInitialPopulation(random)
+
+	for gen := 0; gen < generations; gen++ {
+		newPopulation := make([]Individual, populationSize)
+
+		for i := 0; i < populationSize; i++ {
+			parents := seleccion(random, &population)
+			child := crossover(random, parents[0], parents[1])
+			child = mutate(random, child)
+			newPopulation[i] = child
+		}
+
+		population = newPopulation
+
+		bestIndividual := population[0]
+		for _, ind := range population {
+			if ind.fitness > bestIndividual.fitness {
+				bestIndividual = ind
+			}
+		}
+
+		if bestIndividual.chros == target {
+			fmt.Printf("Solution found in generation %d: %s\n", gen, bestIndividual.chros)
+			break
+		}
+
+		if gen%100 == 0 {
+			fmt.Printf("Generation %d: Best fitness = %d, Best string = %s\n",
+				gen, bestIndividual.fitness, bestIndividual.chros)
+		}
+	}
 }
---
+++ 07-evolutionary-algorithm/go.mod
@@ -0,0 +1,3 @@
+module evolution
+
+go 1.23.3
