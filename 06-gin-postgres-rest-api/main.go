735b74e4797efaf3d8bd9ed7d15d31d909d693c6
alberto-is <albertobarrais12@gmail.com>
2024-11-21 00:59:03 +0100

    chore: Update REST API routes and handlers for users CRUD operations

--- 06-gin-postgres-rest-api/main.go
+++ 06-gin-postgres-rest-api/main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"net/http"
+	servers "06-gin-postgres-rest-api/server"
 
 	"github.com/gin-gonic/gin"
 )
-
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-var users []User = []User{
-	{ID: 1, Name: "Josefa", Age: 20},
-	{ID: 2, Name: "Ramon", Age: 42},
-	{ID: 3, Name: "Laura", Age: 25},
-}
-
-// Note: Change to c.JSON(http.StatusOK, users) for better performance
-func getUsers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, users) // c.JSON(http.StatusOK, users) IS better for performance, but c.IndentedJSON is better for debugging
-}
 
 func main() {
 
 	//go.SetMode(gin.ReleaseMode) // Uncomment this line to set the mode to release
 	router := gin.Default()
-
-	router.GET("/users", getUsers)
-
+	router.GET("/users", servers.GetUsers)
+	router.GET("/users/:id", servers.GetUserByID)
+	router.POST("/users", servers.PostUser)
+	router.PUT("/users", servers.PutUser)
+	router.DELETE("/users/:id", servers.DeleteUser)
 	router.Run(":8080")
 
 }
